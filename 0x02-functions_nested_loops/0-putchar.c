f8a18828d47baa1316e83b18ba81f2b1c4134507
Jack Sparrrow <sparrow@localhost.localdomain>
2022-09-12 14:58:12 +0300

    Functions nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -print _putchar
+ * Return: zero
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - prints alphabets
+ * Return: Alpahbets
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n");
+}
