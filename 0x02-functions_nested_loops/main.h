101b4ccf6adcc2ba93f019fd4dfc381e0e3b14b2
Jack Sparrrow <sparrow@localhost.localdomain>
2022-09-13 18:56:23 +0300

    Updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -15,5 +12,3 @@ int add(int, int);
 void print_to_98(int n);
 void _mul(int mul);
 void print_times_table(int n);
-
-#endif
