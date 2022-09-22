0f80660d67f8e6b99cfb8a57c6a86cacac216612
Jack Sparrrow <sparrow@localhost.localdomain>
2022-09-22 16:52:11 +0300

    Fixed file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,27 @@
 #include "main.h"
+
 /**
- * leet - encodes a string to 1337 encoding scheme
- * @a: pointer to array.
- * Return: the modified array
- **/
-char *leet(char *a)
+ * leet - encodes a string in 1337 encoding scheme
+ * @s: string to be encoded
+ *
+ * Return: the encoded string;
+ */
+char *leet(char *s)
 {
 	int i, j;
-	char let[] = {97, 65, 101, 69, 111, 79, 116, 84, 108, 76};
-	char num[] = {52, 51, 48, 55, 49};
 
-	for (i = 0; a[i] != 0; i++)
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; let[j] != 0; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (a[i] == let[j])
-				a[i] = num[j];
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
 		}
 	}
-	return (a);
+	return (s);
 }
