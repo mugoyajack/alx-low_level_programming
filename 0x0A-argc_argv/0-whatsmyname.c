14c5cf743659bc25a92eb9f9c4af15119aa026f4
Jack Sparrrow <sparrow@localhost.localdomain>
2022-09-30 21:52:28 +0300

    C-argc_argv and static libs

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints its name
+ * @argc: number of args
+ * @argv: args
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
