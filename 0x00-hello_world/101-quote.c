e53d4e08b99fae1fcdf28760931f4883d0e2de08
Jack Mugoya <mugoyajack3@gmail.com>
2023-01-30 10:07:03 +0300

    0x00 C Advanced Tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+/**
+* main - prints a string to the standard error
+* Return: zero
+*/
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (0);
+}
